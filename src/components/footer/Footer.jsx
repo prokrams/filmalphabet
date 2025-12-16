322da6ed8fe7a5d541c97bfa7cc0987a664dffe2
idk pc-mac <depoui@icloud.com>
2025-12-16 22:10:22 +0300

    1.4.5

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,11 @@ export default function Footer() {
     return (
         <div className='footer-container'>
             <p>идея взята с канала <a href='https://youtube.com/@kinoogon'>киноогонь</a></p>
-            <p>•</p>
+            <p className='dot'>•</p>
             <p>сделал <a href='https://t.me/jutchelovek'>jutchelovek</a></p>
-            <p>•</p>
+            <p className='dot'>•</p>
             <p>поиск фильмов возможен благодаря <a href='https://kinopoiskapiunofficial.tech'>API</a></p>
-            <p>•</p>
+            <p className='dot'>•</p>
             <p>помогала <b>лина</b></p>
         </div>
     )
